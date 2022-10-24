68816b8b9a30381ad08ec38944f1455e45d918d8
Moying-moe <741744469@qq.com>
2022-10-24 19:58:04 +0800

    close #17

---
+++ SuperIo/SuperIo.cs
@@ -0,0 +1,36 @@
+﻿/*
+  Copyright (c) Moying-moe All rights reserved. Licensed under the MIT license.
+  See LICENSE in the project root for license information.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperIo
+{
+    public static class SuperIo
+    {
+        /// <summary>
+        /// Initialize all modules.
+        /// </summary>
+        /// <returns></returns>
+        public static bool Initialize()
+        {
+            bool keyboardFlag = SuperKeyboard.Initialize();
+            bool mouseFlag = SuperMouse.Initialize();
+            bool keyhookFlag = SuperKeyHook.Initialize();
+            return keyboardFlag && mouseFlag && keyhookFlag;
+        }
+
+        /// <summary>
+        /// Close all modules.
+        /// </summary>
+        public static void Close()
+        {
+            SuperKeyHook.Close();
+        }
+    }
+}
